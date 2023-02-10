e0b873c77d9287c85084c96b30d4a3a61274df1a
ChangHo_Jin <65750442+jin-chang-ho@users.noreply.github.com>
2023-02-10 14:44:01 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 144 ms, Memory: 23,640 KB -BaekjoonHub

---
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -0,0 +1,77 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.Scanner;
+import java.util.ArrayDeque;
+import java.util.Queue;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		for (int TC = 1; TC <= 10; TC++) {
+			Queue<Integer> queue = new ArrayDeque<>();
+			
+			String number = sc.nextLine();
+			String str = sc.nextLine();
+			String[] strArr = str.split(" ");
+			
+			for (int i = 0; i < strArr.length; i++) {
+				queue.offer(Integer.parseInt(strArr[i]));
+			}
+			
+			int firstNumber = 2;
+			while (firstNumber != 0) {
+				for (int i = 1; i <= 5; i++) {
+					firstNumber = queue.poll();
+					firstNumber = firstNumber - i;
+						
+					if (firstNumber <= 0) {
+						firstNumber = 0;
+						queue.offer(firstNumber);
+						break;
+					}
+						
+					queue.offer(firstNumber);
+				}
+			}
+			
+            System.out.print("#" + TC + " ");
+			for (int i = 0; i < 8; i++) {
+				System.out.print(queue.poll() + " ");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
