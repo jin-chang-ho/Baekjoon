aa182a753baa0d9ae0f8d3ec91004371da386f2f
ChangHo_Jin <65750442+jin-chang-ho@users.noreply.github.com>
2023-02-13 14:08:16 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문1, Time: 159 ms, Memory: 23,492 KB -BaekjoonHub

---
+++ SWEA/D3/1228. ［S／W 문제해결 기본］ 8일차 － 암호문1/［S／W 문제해결 기본］ 8일차 － 암호문1.java
@@ -0,0 +1,76 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String[] args) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		for (int tc = 1; tc <= 10; tc++) {
+			int len = sc.nextInt();
+			
+			List<String> list = new ArrayList<>();
+			
+			for (int i = 0; i < len; i++) {
+				list.add(sc.next());
+			}
+			
+			int N = sc.nextInt();
+			
+			for (int i = 0; i < N; i++) {
+				sc.next();
+				int index = sc.nextInt();
+				int length = sc.nextInt();
+					
+				List<String> temp = new ArrayList<>();
+					
+				for (int j = 0; j < length; j++) {
+					temp.add(sc.next());
+				}
+					
+				list.addAll(index, temp);
+			}
+			
+			System.out.print("#" + tc + " ");
+			
+			for (int i = 0; i < 10; i++) {
+				System.out.print(list.get(i) + " ");
+			}
+			System.out.println();
+		}
+		
+		sc.close();
+	}
+}
