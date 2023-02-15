1da8b86e2b7f53f7ad6169a7cd892a072fbb84b9
ChangHo_Jin <65750442+jin-chang-ho@users.noreply.github.com>
2023-02-16 02:15:03 +0900

    [Silver I] Title: 숨바꼭질, Time: 312 ms, Memory: 31660 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.java
@@ -23,12 +23,6 @@ public class Main {
 		while (true) {
 			int[] check = que.poll();
 			
-			if (!map.containsKey(check[0] * 2) && check[0] != 0 && check[0] * 2 <= 100000) {
-				int[] third = {check[0] * 2, check[1] + 1};
-				que.offer(third);
-				map.put(check[0] * 2, true);
-			}
-			
 			if (!map.containsKey(check[0] - 1) && check[0] - 1 >= 0) {
 				int[] first = {check[0] - 1, check[1] + 1};
 				que.offer(first);
@@ -39,6 +33,12 @@ public class Main {
 				int[] second = {check[0] + 1, check[1] + 1};
 				que.offer(second);
 				map.put(check[0] + 1, true);
+			}
+			
+			if (!map.containsKey(check[0] * 2) && check[0] != 0 && check[0] * 2 <= 100000) {
+				int[] third = {check[0] * 2, check[1] + 1};
+				que.offer(third);
+				map.put(check[0] * 2, true);
 			}
 			
 			if (check[0] == K) {
